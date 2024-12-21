942ccd6692c2ff746e0e89a336ebb97b6840e5dc
Saurabh Kumar <saurabh@MacBookAir.lan>
2024-12-21 21:54:40 +0530

    Added Base

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,25 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
+      "module-resolver",
+      {
+        "root": ["./"],
+        "alias": {
+          "@eLearning": "./src",
+          "@components": "./src/component",
+          "@base": "./src/base",
+          "@services": "./src/service",
+          "@themes": "./src/theme",
+          "@types": "./src/types",
+          "@utils": "./src/utils",
+          "@screens": "./src/screen",
+          "@navigations": "./src/navigation/StackNavigation",
+          "@store": "./src/store",
+          "@navigationTypes": "./src/navigation/NavigationTypes/types",
+          "@navigation-taps": "./src/navigation/BottomNavigation",
+          "@constant": "./src/constant"
+        }
+      }
     ],
   };
 };
